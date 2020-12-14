c07b2de068ddb4aa7d43b78da1ef400d7ac8d6e6
Mohan_Palat <palatster@gmail.com>
2020-12-14 10:28:07 -0500

    Commit 02: Adding src/plans/components/plan.js

---
+++ src/plans/components/plan.js
@@ -0,0 +1,24 @@
+
+// plan.js
+
+import React, { Component } from 'react';
+
+class Plan extends Component {
+  render() {
+    return (
+      <div className="plan">
+        {/* planName, planIsInstitutional, participants */}
+        <h4>{this.props.planName}</h4>
+        <sub>{this.props.planIsInstitutional}</sub>
+        <a href="#" onClick={this.deletePlan}>Delete</a>
+      </div>
+    )
+  }
+
+  deletePlan = (e) => {
+    e.preventDefault();
+    this.props.deletePlan(this.props.id);
+  }
+}
+
+export default Plan;
