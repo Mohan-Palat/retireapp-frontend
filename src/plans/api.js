b9e6e25390f9fc76742485204e68522a4b724b25
Mohan_Palat <palatster@gmail.com>
2020-12-14 10:54:09 -0500

    Commit 04: src/plans/api.js

---
+++ src/plans/api.js
@@ -0,0 +1,24 @@
+
+// api.js
+
+import axios from 'axios';
+
+// INDEX, SHOW, CREATE, UPDATE, DESTROY
+
+// Get All Plans
+const getAllPlans = () => {
+  console.log(`Hitting ALL http://localhost:5000/api/plans`)
+  return axios.get(`http://localhost:5000/api/plans`);
+  // console.log(`Hitting ${process.env.REACT_APP_BASE_URL}/api/plans`)
+  // return axios.get(`${process.env.REACT_APP_BASE_URL}/api/plans`);
+};
+
+// Delete Plan by ID
+const deletePlanByID = (id) => {
+  console.log(`Hitting ID: http://localhost:5000/api/plans/${id}`)
+  return axios.delete(`http://localhost:5000/api/plans/${id}`);
+  // console.log(`Hitting ID: ${process.env.REACT_APP_BASE_URL}/api/plans/${id}`)
+  // return axios.delete(`${process.env.REACT_APP_BASE_URL}/api/plans/${id}`);
+}
+
+export { getAllPlans, deletePlanByID };
