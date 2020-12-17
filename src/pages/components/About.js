30f1045a9770931163cc37feadfea7a7b8695d78
Mohan_Palat <palatster@gmail.com>
2020-12-17 07:26:50 -0500

    Commit 21: Attempting Semantic UI Styling

--- src/pages/components/About.js
+++ src/pages/components/About.js
@@ -2,17 +2,20 @@
 // About.js
 
 import React from 'react';
+import {Header, Image, Segment} from 'semantic-ui-react'
 
 const About = () => {
   return (
     <>
+    <Segment basic inverted padded='very' vertical>
       <h3>About Acme Retirement</h3>
+    </Segment>
       <ul>
-        <li>Expect a safe and peaceful retirement</li>
+        <li>When you invest with us, expect a safe and peaceful retirement</li>
         <li>We have the best plans.</li>
         <li>Acme Investments are associated to Madeoff group of companies and Fly By Night Conglomerate</li>
       </ul>
-    </>
+      </>
   )
 }
 
